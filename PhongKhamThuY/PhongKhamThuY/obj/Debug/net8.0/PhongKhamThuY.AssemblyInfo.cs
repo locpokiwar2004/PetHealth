7fbd8d2d141b1c740058f1d446aacb8709f19923
Kam <126433734+KirietKorange@users.noreply.github.com>
2024-10-23 08:18:20 +0700

    update

--- PhongKhamThuY/PhongKhamThuY/obj/Debug/net8.0/PhongKhamThuY.AssemblyInfo.cs
+++ PhongKhamThuY/PhongKhamThuY/obj/Debug/net8.0/PhongKhamThuY.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhongKhamThuY")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a515e575cbecf41cbf9c1850df717d8033c882e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhongKhamThuY")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhongKhamThuY")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
